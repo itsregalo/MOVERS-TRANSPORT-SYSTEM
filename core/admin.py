79ab4f127e4d4855345d6d3f6dea002d3101f313
retech <itsregalo047@gmail.com>
2021-08-28 21:16:53 +0300

    models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(VehicleCategory)
+admin.site.register(Vehicle)
+admin.site.register(IndividualMembership)
+admin.site.register(SmallScaleMembers)
+admin.site.register(GroupMembership)
+admin.site.register(Driver)
--- core/models.py
+++ core/models.py
@@ -7,13 +7,13 @@ from django.db.models.deletion import DO_NOTHING
 class VehicleCategory(models.Model):
     category_name = models.CharField(max_length=200)
     load_capacity = models.PositiveIntegerField()
-    cost_per_km = models.DecimalField(decimal_places=2)
+    cost_per_km = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.category_name
     
-class Vehicles(models.Model):
-    vehicle_no = models.TextField(max_length=255)
+class Vehicle(models.Model):
+    vehicle_no = models.CharField(max_length=255)
     vehicle_category = models.ForeignKey(VehicleCategory, on_delete=DO_NOTHING)
 
     def __str__(self):
@@ -39,10 +39,18 @@ class SmallScaleMembers(models.Model):
 class GroupMembership(models.Model):
     group_name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    members = models.ManyToOneRel(SmallScaleMembers)
+    members = models.ManyToManyField(SmallScaleMembers)
 
     def __str__(self):
         return self.group_name
 
 
+class Driver(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_no = models.CharField(max_length=13)
+    vehicle = models.ForeignKey(Vehicle, on_delete=DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
