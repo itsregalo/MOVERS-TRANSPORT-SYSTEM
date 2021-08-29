e5c84e9a7542eb528abb9fe68fd0debea8d004c4
retech <itsregalo047@gmail.com>
2021-08-29 21:48:25 +0300

    vehicles

--- core/models.py
+++ core/models.py
@@ -14,6 +14,7 @@ class VehicleCategory(models.Model):
     
 class Vehicle(models.Model):
     vehicle_no = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
     vehicle_category = models.ForeignKey(VehicleCategory, on_delete=DO_NOTHING)
 
     def __str__(self):
