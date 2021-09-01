0e912c209a82c54486396a0c2b3ac57f92afe52f
retech <itsregalo047@gmail.com>
2021-09-01 23:15:28 +0300

    employers and offence cost

--- core/views.py
+++ core/views.py
@@ -3,4 +3,19 @@ from .models import *
 
 # Create your views here.
 
+def IndexView(request, *args, **kwargs):
+    return render(request, 'core/index.html')
+
+def Employers(request, *args, **kwargs):
+    drivers = Driver.objects.all()
+    loaders = Loader.objects.all()
+
+    context = {
+        'drivers':drivers,
+        'loaders':loaders
+    }
+    return render(request, 'core/employers.html', context)
+
+
+
 
